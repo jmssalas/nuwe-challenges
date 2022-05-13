5ebe15980f5a649da7e707e4f1e151720ab2de6c
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2022-05-13 13:03:53 +0200

    Update multiple-3-5.py

--- algorithms/multiple-3-5.py
+++ algorithms/multiple-3-5.py
@@ -4,7 +4,7 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-multiples = [ x for x in range(1, 1000) if x % 3 == 0 or x % 5 == 0]
+multiples = [ x for x in range(1, 1000) if x % 3 == 0 or x % 5 == 0 ]
 print("Multiples of 3 or 5:", multiples)
 sum = sum(multiples)
 print("Sum:", sum)
